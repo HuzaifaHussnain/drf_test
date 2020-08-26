1b94ab03aab1e5ea2f274badab3adcd8f10071f9
Huzaifa Hussnain <huzaifa.hussnain@tintash.com>
2020-08-26 15:10:35 +0500

    Details of the user's profile can now be updated

--- user/serializers.py
+++ user/serializers.py
@@ -1,9 +1,10 @@
 from .models import User, UserProfile
-from rest_framework import serializers
+from rest_framework import serializers, generics
 
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+	""" Serializer for UserProfile Model """
 	class Meta:
 		model = UserProfile
 		fields = ['bio', 'profile_pic']
@@ -25,3 +26,26 @@ class UserSerializer(serializers.ModelSerializer):
 
 	def get_full_name(self, obj):
 		return obj.get_full_name()
+
+
+	def update(self, instance, validated_data):
+		""" overriding the original update function to update the nested objects 
+			Args:
+				validated_data: This contains the updated data for user and userprofile models
+			Return:
+				instance: Returns the updated instance of user model
+
+		"""
+
+		if 'profile' in validated_data:
+			for (key, value) in validated_data['profile'].items():
+				setattr(instance.profile, key, value)
+			validated_data.pop('profile')
+			instance.profile.save()
+
+		for (key, value) in validated_data.items():
+			setattr(instance, key, value)
+		instance.save()
+
+		return instance
+
--- user/views.py
+++ user/views.py
@@ -6,8 +6,11 @@ from .models import User
 
 class UserListUpdateView(generics.ListAPIView, generics.UpdateAPIView):
 	""" View for listing all the user and updating the User information of the current user """
+
 	serializer_class = UserSerializer
 	queryset = User.objects.all()
 
 	def get_object(self):
+		""" Returns the current authenticated user's instance so that 
+		a user can only update his details """
 		return self.request.user
